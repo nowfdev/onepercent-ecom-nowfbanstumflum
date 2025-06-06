1ca6fb2a2beb39025b965caf2e3e2483456422f5
nowfdev <nowf.dev@gmail.com>
2025-06-06 17:34:58 +0700

    Updated UI AboutUs and Contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import Title from "../components/Title";
+import NewsLetterBox from "../components/NewsLetterBox";
 
+import { assets } from "../assets/assets";
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"About"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            recusandae sit vero veritatis itaque? Rem in neque reiciendis
+            facilis maxime nisi, repudiandae, ea placeat reprehenderit libero
+            magnam inventore. Fuga, esse!
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt
+            id eius praesentium aut voluptas repellendus fuga, obcaecati
+            voluptatibus provident porro possimus quam voluptates cum animi
+            dicta reprehenderit corporis. Aspernatur, rerum.
+          </p>
+          <b className="text-gray-600">Our Missions</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            tempore provident error inventore dolorem asperiores sequi.
+            Molestiae tempora, excepturi rem soluta omnis praesentium
+            voluptatibus debitis, consectetur, laudantium fugiat ducimus sequi.
+          </p>
+        </div>
+      </div>
+
+      <div className="text-2xl py-4 ">
+        <Title text1={"Why"} text2={"US"} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Doloremque, aperiam nostrum quod quos inventore ad atque autem?
+            Earum iure voluptas porro esse pariatur, animi suscipit facilis qui
+            saepe praesentium dolorem!
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Doloremque, aperiam nostrum quod quos inventore ad atque autem?
+            Earum iure voluptas porro esse pariatur, animi suscipit facilis qui
+            saepe praesentium dolorem!
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Service:</b>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Doloremque, aperiam nostrum quod quos inventore ad atque autem?
+            Earum iure voluptas porro esse pariatur, animi suscipit facilis qui
+            saepe praesentium dolorem!
+          </p>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Title from "../components/Title";
+import NewsLetterBox from "../components/NewsLetterBox";
+import { assets } from "../assets/assets";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1={"Contact"} text2={"US"} />
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src={assets.contact_img} />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            69 Nguyen Ba Tong <br /> Tan Binh Disctrict
+          </p>
+
+          <p className="text-gray-500">
+            Tel: +84 931 817 054 <br />
+            Email: nowf.dev@gmail.com
+          </p>
+
+          <p className="font-semibold text-xl text-gray-600">Careers</p>
+          <p className="text-gray-600">Learn more about us</p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
+    </div>
+  );
 };
 
 export default Contact;
